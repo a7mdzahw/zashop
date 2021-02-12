3ea0195c369069cddb0580c3c68b37b48a3a0384
a7mdzahw <a7mdzahw@gmail.com>
2021-02-13 01:31:52 +0200

    added product-detail page 7

---
+++ pages/api/products/[id].js
@@ -0,0 +1,19 @@
+const products = [
+  { _id: 1, name: "AirPods", image: "airpods", price: 500, rating: 4.5 },
+  { _id: 2, name: "Camera", image: "camera", price: 500, rating: 4.5 },
+  { _id: 3, name: "Mouse", image: "mouse", price: 500, rating: 4.5 },
+  {
+    _id: 4,
+    name: "Play Station",
+    image: "playstation",
+    price: 500,
+    rating: 4.5,
+  },
+  { _id: 5, name: "Phone", image: "phone", price: 500, rating: 4.5 },
+  { _id: 6, name: "Alexa", image: "alexa", price: 500, rating: 4.5 },
+];
+
+export default (req, res) => {
+  const product = products.find((p) => p._id == req.query.id);
+  res.status(200).json(product);
+};
