a07013d69859fe0686a3c85a405bc52f266c2b36
a7mdzahw <a7mdzahw@gmail.com>
2021-02-13 01:58:02 +0200

    added product-detail page 11

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -6,7 +6,7 @@ const Topbar = () => {
       <div className="container">
         <h1 className="navbar-brand mx-2">
           <Link href="/">
-            <div className="d-flex gap-1">
+            <div className="d-flex gap-2">
               <i className="bi bi-shop"></i>
               <a className="nav-link">ZaShop</a>
             </div>
